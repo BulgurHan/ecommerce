1af9e3b6c235de4b309651ab1b9f90cfe20bc9b7
BulgurHan <mertgurkan034@gmail.com>
2025-04-05 11:52:16 +0300

    seo yapılandırması yapıldı ama product ekranı kaldı!

--- page/models.py
+++ page/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+
+class Email(models.Model):
+    email = models.EmailField(unique=True)
+
+    def clean(self):
+        if Email.objects.exists() and not self.pk:
+            raise ValidationError("Sadece bir tane e-posta adresi kaydedilebilir.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean() 
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
+    
+    class Meta:
+        verbose_name = "Email"
+        verbose_name_plural = "Email"
+
+
+class Kargo(models.Model):
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Tutar'
+    )
+
+    def clean(self):
+        if Kargo.objects.exists() and not self.pk:
+            raise ValidationError("Sadece bir tane kargo fiyatı tanımlanabilir.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean() 
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.price} TL"
+    
+    class Meta:
+        verbose_name = "Kargo Fiyatlandırması"
+        verbose_name_plural = "Kargo Fiyatlandırması"
+
